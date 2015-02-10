2eb13960b1e1811f1311edd375508f45065c4c1a
Leandrow <leandrow@01s.me>
2015-02-10 08:05:14 -0200

    Add uikit framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,18 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		uglify: {'public/js/main.js': 'develop/js/*.js'},
+		uglify: {
+			'public/js/main.js': 'develop/js/*.js',
+			'public/js/components/uikit.min.js': 'develop/js/components/*.js'
+		},
 		less: {
 			development: {
 				options: {compress: true},
 				files: {"public/css/main.css": "develop/css/*.less"}
 			}
+		},
+		cssmin: {
+			'public/css/components/uikit.min.css': 'develop/css/**/*.css'
 		},
 		watch: { 
 			files: ['develop/js/main.js', 'develop/css/main.less'],
@@ -17,7 +23,9 @@ module.exports = function(grunt) {
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
 	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('css', ['cssmin']);
 };
