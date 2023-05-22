867a67b78ed8ab0513c83b9796a349eb401490cf
Leandro Silva <leandrow01s@gmail.com>
2023-05-21 23:35:10 -0300

    adds feather-icons

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,6 @@
 import '../styles/main.css'
+import '../../node_modules/feather-icons/dist/feather'
 import { setupCounter } from './counter.js'
 
+feather.replace()
 setupCounter(document.querySelector('#counter'))
