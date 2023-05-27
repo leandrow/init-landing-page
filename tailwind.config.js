39e4f0985c48784d868817637d23f8359620e0b8
Leandro Silva <leandrow01s@gmail.com>
2023-05-27 02:25:16 -0300

    set breakpoint: sm

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: ["./dev/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '480px',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
