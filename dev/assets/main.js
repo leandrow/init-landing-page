51290fbfb01817c4f24e449a670539d5a7fcf1cf
Leandro Silva <leandrow01s@gmail.com>
2023-11-14 00:55:12 -0300

    add @phosphor-icons

--- dev/assets/main.js
+++ dev/assets/main.js
@@ -2,6 +2,7 @@ import '../styles/main.css';
 import feather from 'feather-icons';
 import { setupCounter } from './counter.js';
 import './darkmode';
+import '@phosphor-icons/web/regular';
 
 feather.replace();
 setupCounter(document.querySelector('#counter'));
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 export default {
   root: 'dev',
   build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/woff|woff2|svg|ttf/.test(extType)) {
+            extType = 'fonts';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
     outDir: '../build',
   },
   base: '/init-setup/',
