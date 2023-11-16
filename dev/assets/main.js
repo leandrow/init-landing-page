e0ae578643dd4b2cce9793fbe2dd70d2edf735a0
Leandro Silva <leandrow01s@gmail.com>
2023-11-15 21:59:56 -0300

    components button added

--- dev/assets/main.js
+++ dev/assets/main.js
@@ -1,7 +1,4 @@
 import '../styles/main.css';
-import { setupCounter } from './counter.js';
 import './darkmode';
 import '@phosphor-icons/web/regular';
 import '@fontsource-variable/inter';
-
-setupCounter(document.querySelector('#counter'));
