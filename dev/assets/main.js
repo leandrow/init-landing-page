60aa8a92d75c43a3640f314f75eb50b8a0dc5ddd
Leandro Silva <leandrow01s@gmail.com>
2023-11-15 21:31:26 -0300

    remove feather icons

--- dev/assets/main.js
+++ dev/assets/main.js
@@ -1,9 +1,7 @@
 import '../styles/main.css';
-import feather from 'feather-icons';
 import { setupCounter } from './counter.js';
 import './darkmode';
 import '@phosphor-icons/web/regular';
 import '@fontsource-variable/inter';
 
-feather.replace();
 setupCounter(document.querySelector('#counter'));
