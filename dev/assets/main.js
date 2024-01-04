4d4e1e7526ce6643700708728c279751bfc9309d
Leandro Silva <leandrow01s@gmail.com>
2024-01-04 04:09:40 -0300

    Responsive menu (#11)

--- dev/assets/main.js
+++ dev/assets/main.js
@@ -2,3 +2,13 @@ import '../styles/main.sass';
 import '@fontsource-variable/inter';
 import '@phosphor-icons/web/regular';
 import './darkmode';
+
+// Responsive menu
+const btn = document.querySelector('.responsive-btn');
+const menu = document.querySelector('.responsive-menu');
+
+if (btn && menu) {
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+}
